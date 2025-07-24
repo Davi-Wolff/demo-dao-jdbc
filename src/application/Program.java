bb2991ea3552efb826a4e4c39d7868605bf3cc4c
Davi-Wolff <daviwolffdeabreu@gmail.com>
2025-07-23 23:01:39 -0300

    feature: adding the 'SellerDaoJDBC' and the 'DaoFactory'

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.DAO.DaoFactory;
+import model.DAO.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,9 @@ public class Program {
 	
 	Department dp = new Department(1,"Books");
 	Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dp);
+	
+SellerDAO sellerDao = DaoFactory.createSellerDAO();
+// don't need to do the "new Seller()" and don't need to do an instance of the DaoFactory cause the method is static.
 	
 	System.out.println(seller);
 	}
