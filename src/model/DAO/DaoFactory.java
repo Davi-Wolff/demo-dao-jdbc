dc296277a9e6855658c10d719cc7e5f5819e6141
Davi-Wolff <daviwolffdeabreu@gmail.com>
2025-07-28 17:01:37 -0300

    feature: adding DepartmentDaoJDBC methods (and adding the class into the DaoFactory), and adding the update, insert and deleting methods into the SellerDaoJDBC class

--- src/model/DAO/DaoFactory.java
+++ src/model/DAO/DaoFactory.java
@@ -1,12 +1,17 @@
 package model.DAO;
 
 import db.DB;
+import model.DAO.impl.DepartmentDaoJDBC;
 import model.DAO.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
 	public static SellerDAO createSellerDAO() {
 		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
+	public static DepartmentDAO createDepartmentDAO() {
+		return new DepartmentDaoJDBC(DB.getConnection());
 	}
 	
 }
--- src/model/DAO/impl/SellerDaoJDBC.java
+++ src/model/DAO/impl/SellerDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.Map;
 
 import db.DB;
 import db.DbException;
+import db.DbIntegrityException;
 import model.DAO.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
@@ -25,19 +27,88 @@ public class SellerDaoJDBC implements SellerDAO {
 
 	@Override
 	public void insert(Seller seller) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					  "INSERT INTO seller "
+					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES "
+					+ "(?,?,?,?,?)",
+					Statement.RETURN_GENERATED_KEYS);
+			st.setString(1, seller.getName());
+			st.setString(2, seller.getEmail());
+			st.setDate(3, (new java.sql.Date(seller.getBirthDate().getTime())));
+			st.setDouble(4, seller.getBaseSalary());
+			st.setInt(5, seller.getDepartment().getId());
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if(rs.next()) {
+					int id = rs.getInt(1);
+					seller.setId(id);
+					DB.closeResultSet(rs);
+				}
+			} else {
+				throw new DbException("Unexpected error! No rows were affected!");
+			}
+			
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 
 	}
 
 	@Override
 	public void update(Seller seller) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					  "UPDATE seller "
+					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+					+ "WHERE Id = ?");
+					
+			st.setString(1, seller.getName());
+			st.setString(2, seller.getEmail());
+			st.setDate(3, (new java.sql.Date(seller.getBirthDate().getTime())));
+			st.setDouble(4, seller.getBaseSalary());
+			st.setInt(5, seller.getDepartment().getId());
+			st.setInt(6, seller.getId());
+			
+			st.executeUpdate();
+			
+					}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		} 
+		 finally {
+			DB.closeStatement(st);
+		}
 
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					  "DELETE FROM seller WHERE Id = ?");
+					
+			st.setInt(1,id);
+			
+			int rowsAffected = st.executeUpdate();
+			if(rowsAffected == 0) {
+				throw new DbException("This Id don't exist! No seller was deleted!");
+			}
+			
+					}catch(SQLException e) {
+			throw new DbIntegrityException(e.getMessage());
+		} 
+		 finally {
+			DB.closeStatement(st);
+		}
 
 	}
 
